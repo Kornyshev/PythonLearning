bf39fe762be7654b664426b3bdc7971eb5c1363c
Evgeniy Kornyshev <Evgeniy_Kornyshev@epam.com>
2023-03-28 23:42:54 +0300

    Lecture 7 HW is finished

---
+++ lecture7/hw1.py
@@ -0,0 +1,89 @@
+# Create a new database named "films_db".
+# Use the SQLAlchemy library to create the database and tables in Python.
+# Part 1: Setting up the Database
+# Create one table for films, with the following columns:
+#     films table:
+#         id (integer, primary key)
+#         title (string)
+#         director (string)
+#         release year (integer)
+# Part 2: Manipulating with Database
+#     Create script that:
+#         Add 3 film to the film table.
+#         Update 1 film
+#         Print data from table
+#         Delete all data from table
+from typing import Iterable
+
+from sqlalchemy import *
+from sqlalchemy.orm import *
+
+Base = declarative_base()
+engine = create_engine('sqlite:///films_db.db')
+
+
+class Film(Base):
+    __tablename__ = 'films'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255))
+    director = Column(String(255))
+    release_year = Column(Integer)
+
+    def __str__(self):
+        return f"Title: {self.title}, director: {self.director}, release year: {self.release_year}"
+
+
+Base.metadata.create_all(engine)
+
+
+def print_all_films():
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+    all_films = session.query(Film).all()
+    if len(all_films) == 0:
+        print("\nTable 'films' is empty")
+    else:
+        print("\nAll films in the table:")
+        for elem in session.query(Film).all():
+            print(elem)
+
+
+def clear_films_table():
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+    session.query(Film).delete()
+    session.commit()
+
+
+def add_films_to_db(films: Iterable[Film]):
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+    for film in films:
+        session.add(film)
+    session.commit()
+
+
+def update_film_year_by_title(title, year):
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+    session.query(Film).filter(Film.title == title).update({'release_year': year})
+    session.commit()
+
+
+if __name__ == '__main__':
+    print_all_films()
+    films_list = [
+        Film(title='The Shawshank Redemption', director='Frank Darabont', release_year=1994),
+        Film(title='The Godfather', director='Francis Ford Coppola', release_year=1972),
+        Film(title='The Dark Knight', director='Christopher Nolan', release_year=2022),  # Correct year: 2008
+        Film(title='Forrest Gump', director='Robert Zemeckis', release_year=1994),
+        Film(title='Inception', director='Christopher Nolan', release_year=2010),
+        Film(title='The Matrix', director='The Wachowski Brothers', release_year=1999),
+    ]
+    add_films_to_db(films_list)
+    print_all_films()
+    update_film_year_by_title('The Dark Knight', 2008)
+    print_all_films()
+    clear_films_table()
+    print_all_films()
